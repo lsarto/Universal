f9cb0c1d02404d47730560751aefb8ed631bc43d
LINO\Lino <lino.sarto@gmail.com>
2018-01-29 19:40:49 +0100

    no message

--- src/main/java/com/popeyestore/PopeyestoreApplication.java
+++ src/main/java/com/popeyestore/PopeyestoreApplication.java
@@ -41,18 +41,30 @@ public class PopeyestoreApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		User user1 = new User();
-		user1.setFirstName("John");
-		user1.setLastName("Adams");
-		user1.setUsername("j");
-		user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
-		user1.setEmail("lino.sarto@gmail.com");
+		user1.setUsername("admin");
+		user1.setPassword(SecurityUtility.passwordEncoder().encode("admin"));
+		user1.setEmail("admin@gmail.com");
 		Set<UserRole> userRoles = new HashSet<>();
 		Role role1= new Role();
-		role1.setRoleId(1);
-		role1.setName("ROLE_USER");
+		role1.setRoleId(0);
+		role1.setName("ROLE_ADMIN");
 		userRoles.add(new UserRole(user1, role1));
 		
 		userService.createUser(user1, userRoles);
+		
+		User user2 = new User();
+		user2.setFirstName("John");
+		user2.setLastName("Adams");
+		user2.setUsername("j");
+		user2.setPassword(SecurityUtility.passwordEncoder().encode("p"));
+		user2.setEmail("lino.sarto@gmail.com");
+		userRoles = new HashSet<>();
+		Role role2= new Role();
+		role2.setRoleId(1);
+		role2.setName("ROLE_USER");
+		userRoles.add(new UserRole(user2, role2));
+		
+		userService.createUser(user2, userRoles);
 		
 //		Type type1 = new Type();
 //		Type type2 = new Type();
